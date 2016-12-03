f4359862c36af72a0e4726a848b553ff0ae69738
Heet Vachhani <heet_vachhani@hotmail.com>
2016-12-03 17:53:03 -0600

    Crawling using crawler4j completed

--- src/Controller.java
+++ src/Controller.java
@@ -5,64 +5,32 @@ import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
 
 public class Controller {
+
 	public static void main(String[] args) throws Exception {
 		/*
 		 * crawlStorageFolder is a folder where intermediate crawl data is
 		 * stored.
 		 */
-		String crawlStorageFolder = "/Users/Heet/Downloads/soccerData";
-
+		String crawlStorageFolder = "/Users/Heet/Downloads/soccerData/tempdocs";
 		/*
 		 * numberOfCrawlers shows the number of concurrent threads that should
 		 * be initiated for crawling.
 		 */
-		int numberOfCrawlers = 6;
+		int numberOfCrawlers = 10;
 
 		CrawlConfig config = new CrawlConfig();
 
 		config.setCrawlStorageFolder(crawlStorageFolder);
 
-		/*
-		 * Be polite: Make sure that we don't send more than 1 request per
-		 * second (1000 milliseconds between requests).
-		 */
 		config.setPolitenessDelay(1000);
 
-		/*
-		 * You can set the maximum crawl depth here. The default value is -1 for
-		 * unlimited depth
-		 */
 		config.setMaxDepthOfCrawling(2);
 
-		/*
-		 * You can set the maximum number of pages to crawl. The default value
-		 * is -1 for unlimited number of pages
-		 */
-		config.setMaxPagesToFetch(30);
-
-		/**
-		 * Do you want crawler4j to crawl also binary data ? example: the
-		 * contents of pdf, or the metadata of images etc
-		 */
+		config.setMaxPagesToFetch(100200);
+		
 		config.setIncludeBinaryContentInCrawling(false);
 
-		/*
-		 * Do you need to set a proxy? If so, you can use:
-		 * config.setProxyHost("proxyserver.example.com");
-		 * config.setProxyPort(8080);
-		 *
-		 * If your proxy also needs authentication:
-		 * config.setProxyUsername(username); config.getProxyPassword(password);
-		 */
-
-		/*
-		 * This config parameter can be used to set your crawl to be resumable
-		 * (meaning that you can resume the crawl from a previously
-		 * interrupted/crashed crawl). Note: if you enable resuming feature and
-		 * want to start a fresh crawl, you need to delete the contents of
-		 * rootFolder manually.
-		 */
-		config.setResumableCrawling(false);
+		config.setResumableCrawling(true);
 
 		/*
 		 * Instantiate the controller for this crawl.
@@ -77,13 +45,44 @@ public class Controller {
 		 * URLs that are fetched and then the crawler starts following links
 		 * which are found in these pages
 		 */
-		controller.addSeed("http://www.wimbledon.com/index.html");
-		//controller.addSeed("http://www.atpworldtour.com/");
-
-		/*
-		 * Start the crawl. This is a blocking operation, meaning that your code
-		 * will reach the line after this only when crawling is finished.
-		 */
+		controller.addSeed("https://en.wikibooks.org/wiki/Football_(Soccer)");
+		controller.addSeed("https://en.wikibooks.org/wiki/Football_(Soccer)/The_Leagues_and_Teams#FIFA_Divisions");
+		controller.addSeed("http://www.footballgroundguide.com/");
+		controller.addSeed("http://www.worldsoccer.com/");
+		controller.addSeed("http://www.fifa.com/");
+		controller.addSeed("http://www.footytube.com/");
+		controller.addSeed("http://www.conmebol.com/en");
+		controller.addSeed("http://www.the-afc.com/");
+		controller.addSeed("http://www.cafonline.com/");
+		controller.addSeed("http://www.oceaniafootball.com/ofc/");
+		controller.addSeed("http://www.laliga.es/en");
+		controller.addSeed("http://www.concacaf.com/");
+		controller.addSeed("http://www.espnfc.us/");
+		controller.addSeed("http://www.goal.com/en-us/");
+		controller.addSeed("http://www.uefa.com/");
+		controller.addSeed("http://www.tribalfootball.com/");
+		controller.addSeed("http://www.foxsports.com/soccer/news");
+		controller.addSeed("http://www.soccernews.com/");
+		controller.addSeed("https://sports.yahoo.com/soccer/");
+		controller.addSeed("http://bleacherreport.com/world-football");
+		controller.addSeed("http://sportslens.com/");
+		controller.addSeed("http://www.soccerbase.com/");
+		controller.addSeed("http://www.101greatgoals.com/");
+		controller.addSeed("http://www.football365.com/");
+		controller.addSeed("http://www.footballfilter.com/");
+		controller.addSeed("http://worldsoccertalk.com/");
+		controller.addSeed("https://www.msn.com/en-us/sports/soccer");
+		controller.addSeed("https://www.theguardian.com/football");
+		controller.addSeed("http://www.fourfourtwo.com/");
+		controller.addSeed("http://edition.cnn.com/sport/football");
+		controller.addSeed("http://athenasportsnet.com/soccer/");
+		controller.addSeed("http://www.soccer24.com/");
+		controller.addSeed("https://www.premierleague.com/");
+		controller.addSeed("http://www.offthepost.info/");
+		controller.addSeed("http://www.vitalfootball.co.uk/");
+		
+		
 		controller.start(MyCrawler.class, numberOfCrawlers);
 	}
+
 }
--- src/MyCrawler.java
+++ src/MyCrawler.java
@@ -1,5 +1,6 @@
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
-import java.io.PrintWriter;
 import java.util.Set;
 import java.util.regex.Pattern;
 
@@ -10,70 +11,54 @@ import edu.uci.ics.crawler4j.url.WebURL;
 
 public class MyCrawler extends WebCrawler {
 
-	private static String path = "/Users/Heet/Downloads/soccerData";
-
+	private static String path = "/Users/Heet/Downloads/soccerData/tempdocs/";
 	private static final Pattern FILTERS = Pattern
 			.compile(".*(\\.(css|js|bmp|gif|jpe?g|png|tiff?|mid|mp2|mp3|mp4|wav|avi|mov|mpeg|ram|m4v|pdf"
 					+ "|rm|smil|wmv|swf|wma|zip|rar|gz))$");
 
 	@Override
 	public boolean shouldVisit(Page referringPage, WebURL url) {
-		// boolean acceptUrl;
 		String href = url.getURL().toLowerCase();
-		return !FILTERS.matcher(href).matches() && href.startsWith("http:");
-		//
-		// if (href.startsWith("http:")) {
-		// acceptUrl = true;
-		// if (href.endsWith("xml")) {
-		// acceptUrl = false;
-		// } else if (href.contains("s.teoma.com"))
-		// acceptUrl = false;
-		// else if (href.contains("store.fifa.com") ||
-		// href.contains("oldungvar") || href.contains("ajkids.com"))
-		// acceptUrl = false;
-		// } else {
-		// acceptUrl = false;
-		// System.out.println("DEBUG: Filtered url: '" + url + "'");
-		// }
-		//
-		// return acceptUrl;
+		return  !FILTERS.matcher(href).matches() && href.startsWith("http:") && !href.endsWith("xml") && !href.contains("teoma.com");
 	}
 
 	@Override
 	public void visit(Page page) {
 		int docid = page.getWebURL().getDocid();
 		String url = page.getWebURL().getURL();
-		String domain = page.getWebURL().getDomain();
-		String parentUrl = page.getWebURL().getParentUrl();
-		String anchor = page.getWebURL().getAnchor();
-
-		int parentDocid = page.getWebURL().getParentDocid();
 		try {
-			PrintWriter out = new PrintWriter(new FileWriter(path + "/doc" + docid, true));
-
-			out.print("{\"DocID\":" + docid);
-			out.print(",\"URL\":\"" + url + "\"");
-			out.print(",\"ParentID\":" + parentDocid);
-			out.print(",\"ParentURL\":\"" + parentUrl + "\"");
-			out.print(",\"Domain\":\"" + domain + "\"");
-			out.print(",\"Anchor\":\"" + anchor + "\"");
+			File file = new File(path + docid);
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			System.out.println(url);
 
 			if (page.getParseData() instanceof HtmlParseData) {
 				HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
 				String text = htmlParseData.getText().replaceAll("\\s+", " ");
 				String title = htmlParseData.getTitle();
+				bw.write("Title:" + title + "::");
+				bw.newLine();
+				bw.write("URL:" + url + "::");
+				bw.newLine();
+				bw.write(text + "::");
+				bw.newLine();
 				Set<WebURL> outgoingLinks = htmlParseData.getOutgoingUrls();
+				bw.write("Outlinks:");
 
-				out.print(",\"Title\":\"" + title + "\"");
-				out.print(",\"NoOfOutgoingLinks\":" + outgoingLinks.size());
-				out.print(",\"Text\":\"" + text + "\"}");
-
+				StringBuffer sb = new StringBuffer();
+				for (WebURL link : outgoingLinks) {
+					String linkHref = link.getURL();
+					sb.append(linkHref);
+					sb.append(", ");
+					if (linkHref.isEmpty())
+						continue;
+				}
+				bw.write(sb.toString());
+				bw.close();
 			}
-			out.close();
 
 		} catch (Exception e) {
 			System.out.println("Invalid path to write!!");
 		}
-
 	}
 }
